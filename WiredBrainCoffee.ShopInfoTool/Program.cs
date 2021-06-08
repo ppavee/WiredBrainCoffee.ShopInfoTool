69184f6d6547a2dabc97d3fd47125a87403b8ea2
Paavo Rantala <paavorantala94@gmail.com>
2021-06-08 21:27:21 +0300

    Implement help command

--- WiredBrainCoffee.ShopInfoTool/Program.cs
+++ WiredBrainCoffee.ShopInfoTool/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using WiredBrainCoffee.DataAccess;
 
 namespace WiredBrainCoffee.ShopInfoTool
 {
@@ -7,6 +8,32 @@ namespace WiredBrainCoffee.ShopInfoTool
         static void Main(string[] args)
         {
             Console.WriteLine("Wired Brain Coffee - Shop Info Tool!");
+
+            Console.WriteLine("Write 'help' to list available coffee shop commands, " +
+              "write 'quit' to exit application");
+
+            var coffeeShopDataProvider = new CoffeeShopDataProvider();
+
+            while (true)
+            {
+                var line = Console.ReadLine();
+
+                //if (string.Equals("quit", line, StringComparison.OrdinalIgnoreCase))
+                //{
+                //    break;
+                //}
+
+                var coffeeShops = coffeeShopDataProvider.LoadCoffeeShops();
+
+                if (string.Equals("help", line, StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("> Available coffee shop commands:");
+                    foreach (var coffeeShop in coffeeShops)
+                    {
+                        Console.WriteLine($"> " + coffeeShop.Location);
+                    }
+                }
+            }
         }
     }
 }
