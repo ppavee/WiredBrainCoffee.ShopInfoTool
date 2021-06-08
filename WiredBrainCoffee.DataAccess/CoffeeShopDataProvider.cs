a5572cc6a908f0ccb5e35633763514bb92619b53
Paavo Rantala <paavorantala94@gmail.com>
2021-06-08 21:26:37 +0300

    Add data access project

---
+++ WiredBrainCoffee.DataAccess/CoffeeShopDataProvider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WiredBrainCoffee.DataAccess.Model;
+
+namespace WiredBrainCoffee.DataAccess
+{
+    public class CoffeeShopDataProvider
+    {
+        public IEnumerable<CoffeeShop> LoadCoffeeShops()
+        {
+            yield return new CoffeeShop { Location = "Frankfurt", BeansInStockInKg = 107 };
+            yield return new CoffeeShop { Location = "Freiburg", BeansInStockInKg = 23 };
+            yield return new CoffeeShop { Location = "Munich", BeansInStockInKg = 56 };
+        }
+    }
+}
---
+++ WiredBrainCoffee.DataAccess/Model/CoffeeShop.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WiredBrainCoffee.DataAccess.Model
+{
+    public class CoffeeShop
+    {
+        public string Location { get; set; }
+        public int BeansInStockInKg { get; set; }
+    }
+}
