3a4acf581de3b8893b99d26cd8ddec11c9088ad8
Paavo Rantala <paavorantala94@gmail.com>
2021-06-09 10:07:03 +0300

    Introduce command handlers

---
+++ WiredBrainCoffee.ShopInfoTool/HelpCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WiredBrainCoffee.DataAccess.Model;
+
+namespace WiredBrainCoffee.ShopInfoTool
+{
+    internal class HelpCommandHandler : ICommandHandler
+    {
+        private IEnumerable<CoffeeShop> coffeeShops;
+
+        public HelpCommandHandler(IEnumerable<CoffeeShop> coffeeShops)
+        {
+            this.coffeeShops = coffeeShops;
+        }
+
+        public void HandleCommand()
+        {
+            Console.WriteLine("> Available coffee shop commands:");
+            foreach (var coffeeShop in coffeeShops)
+            {
+                Console.WriteLine($"> {coffeeShop.Location}");
+            }
+        }
+    }
+}
--- WiredBrainCoffee.ShopInfoTool/Program.cs
+++ WiredBrainCoffee.ShopInfoTool/Program.cs
@@ -25,19 +25,12 @@ namespace WiredBrainCoffee.ShopInfoTool
 
                 var coffeeShops = coffeeShopDataProvider.LoadCoffeeShops();
 
-                if (string.Equals("help", line, StringComparison.OrdinalIgnoreCase))
-                {
-                    Console.WriteLine("> Available coffee shop commands:");
-                    foreach (var coffeeShop in coffeeShops)
-                    {
-                        Console.WriteLine($"> " + coffeeShop.Location);
-                    }
+                var commandHandler =
+                  string.Equals("help", line, StringComparison.OrdinalIgnoreCase)
+                  ? new HelpCommandHandler(coffeeShops) as ICommandHandler
+                  : new CoffeeShopCommandHandler(coffeeShops, line);
 
-                    if(coffeeShops == null)
-                    {
-                        Console.WriteLine($"> No coffee shops found.");
-                    }
-                }
+                commandHandler.HandleCommand();
             }
         }
     }
